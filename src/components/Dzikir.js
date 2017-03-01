d92726debde96a5b749f4aef6f9784ec3f39cb4d
Muhammad Miftah Firdaus <muh.miftah.firdaus@gmail.com>
2017-03-01 14:21:06 +0700

    Repo heroku

--- src/components/Dzikir.js
+++ src/components/Dzikir.js
@@ -25,7 +25,7 @@ export default class Dzikir extends React.Component {
 				<div className="col-md-4 col-md-offset-4">
 					<h1>{this.state.count}</h1>
 					<div className="col-md-3 col-xs-2 col-xs-offset-2 col-md-offset-2">
-						<button className="btn btn-primary" onClick={this.handleCount.bind(this)}><b>Count</b></button>
+						<button className="btn btn-primary" onClick={this.handleCount.bind(this)}><b>Counts</b></button>
 					</div>
 					<div className="col-md-3 col-xs-2 col-xs-offset-2 col-md-offset-2">
 						<button className="btn btn-default" onClick={this.handleReset.bind(this)}><b>Reset</b></button>
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
 module.exports = {
-	entry: {
-		app: ["./src/main.js"]
-	},
+	entry: './src/main.js',
 	output: {
-		path: __dirname + "./public/code",
-		publicPath: "/code/",
-		filename: "bundle.js"
+		path: __dirname + '/public/code',
+		filename: 'bundle.js'
+	},
+	devServer: {
+		port: 3000,
+		contentBase: __dirname + '/public',
+		inline: true
 	},
 	module: {
 	    loaders: [{
 	      test: /\.js$/,
 	      exclude: /node_modules/,
 	      loader: "babel-loader",
-	      include: __dirname,
 	      query: {
 	        presets: [ 'es2015', 'react' ]
 	      }
