fe8e5cd8c9cde4ad4707375635fdd8b24dd0e34c
Muhammad Miftah Firdaus <muh.miftah.firdaus@gmail.com>
2017-03-02 07:53:30 +0700

    repo heroku

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,16 @@ module.exports = {
 		path: __dirname + '/public/code',
 		filename: 'bundle.js'
 	},
-	devServer: {
-		port: 3000,
-		contentBase: __dirname + '/public',
-		inline: true
-	},
+	// devServer: {
+	// 	port: 3000,
+	// 	contentBase: __dirname + '/public',
+	// 	inline: true
+	// },
 	module: {
 	    loaders: [{
 	      test: /\.js$/,
 	      exclude: /node_modules/,
-	      loader: "babel-loader",
+	      loader: 'babel-loader',
 	      query: {
 	        presets: [ 'es2015', 'react' ]
 	      }
